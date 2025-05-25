e6a055d8efac2c7596d0815f4bf1e8a81b776ff8
Ric3cir121 <ric3cir121@gmail.com>
2025-05-25 22:18:37 +0200

    Removed unused code

--- audio-screen-share@ric3cir121.org/extension.js
+++ audio-screen-share@ric3cir121.org/extension.js
@@ -15,23 +15,15 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
+import Gio from 'gi://Gio';
+import St from 'gi://St';
+
 import {Extension, gettext, ngettext, pgettext} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
 export default class AudioScreenSharing extends Extension {
     enable() {
-        // Add a menu item to open the preferences window
-        this._indicator.menu.addAction(gettext('Preferences'),
-            () => this.openPreferences());
-
-        // Create a new GSettings object, and bind the "show-indicator"
-        // setting to the "visible" property.
-        this._settings = this.getSettings();
-        this._settings.bind('show-indicator', this._indicator, 'visible', Gio.SettingsBindFlags.DEFAULT);
-
-        // Watch for changes to a specific setting
-        this._settings.connect('changed::show-indicator', (settings, key) => {
-            console.debug(`${key} = ${settings.get_value(key).print(true)}`);
-        });
     }
 
     disable() {
